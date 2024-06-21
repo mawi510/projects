987e21912bcfd8fe361ba6ea22e403ee0c7ac91c
mawi510 <117122865+mawi510@users.noreply.github.com>
2024-06-21 19:04:36 -0400

    Update test_update_nfl_data_s3.py

--- PredictingNFLGames/test_update_nfl_data_s3.py
+++ PredictingNFLGames/test_update_nfl_data_s3.py
@@ -1,40 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[13]:
-
-
-'''
-In this notebook, we'll practice writing to our S3 bucket, and reading a file from said S3 bucket
-Let's import our packages
-'''
+#Let's import our requirements
 
 import pandas as pd
 import boto3
 
 
-# In[14]:
-
-
 #Let's pull our S3 Credentials, and specify the S3 bucket we wish to write to
 
-get_ipython().run_line_magic('store', '-r aws_access_key_id')
-get_ipython().run_line_magic('store', '-r aws_secret_access_key')
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 aws_s3_bucket = "latestnfldata"
 
 
-# In[15]:
-
-
-#Let's preview our csv file that we want to send to S3
-
-df = pd.read_csv('nfl_training_data_2023.csv')
-df.head()
-
-
-# In[16]:
-
+#Let's pull our csv
+df = pd.read_csv('PredictingNFLGames/nfl_training_data_2023.csv')
 
 #Now we can use pandas to write this file to S3
 
@@ -46,33 +25,6 @@ df.to_csv(
         "secret": aws_secret_access_key,
         }
 )
-
-
-# In[17]:
-
-
-#Upload was successful
-
-
-# In[19]:
-
-
-#Now we can read the file from the S3 bucket as such
-
-
-df = pd.read_csv(
-    f"s3://{aws_s3_bucket}/nfl_training_data_2023.csv",
-    storage_options={
-        "key": aws_access_key_id,
-        "secret": aws_secret_access_key,
-        }
-)
-
-df.head()
-
-
-# In[ ]:
-
 
 
 
