c9d818ceb399bf8059cc25cc83ff0f3eff1f1baf
mawi510 <117122865+mawi510@users.noreply.github.com>
2024-07-20 11:42:01 -0400

    Re-writing as .pem is needed

--- PredictingNFLGames/run_certbot_update.py
+++ PredictingNFLGames/run_certbot_update.py
@@ -1,12 +1,15 @@
 import paramiko
 import os
 
-def run_remote_command(hostname, username, key, command):
+def run_remote_command(hostname, username, pem_file_path, command):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
     try:
-        ssh.connect(hostname, username=username, key_filename=key)
+        # Load the private key from the .pem file
+        key = paramiko.RSAKey.from_private_key_file(pem_file_path)
+        
+        ssh.connect(hostname, username=username, pkey=key)
         stdin, stdout, stderr = ssh.exec_command(command)
         print(stdout.read().decode())
         print(stderr.read().decode())
@@ -16,7 +19,7 @@ def run_remote_command(hostname, username, key, command):
 if __name__ == "__main__":
     hostname = os.getenv("EC2_PUBLIC_IP")
     username = os.getenv("EC2_USERNAME")
-    key = os.getenv("EC2_SSH_KEY")
+    pem_file_path = "key.pem"
     command = "sudo certbot renew --nginx --cert-name promatchpredict.com"
     
-    run_remote_command(hostname, username, key, command)
+    run_remote_command(hostname, username, pem_file_path, command)
